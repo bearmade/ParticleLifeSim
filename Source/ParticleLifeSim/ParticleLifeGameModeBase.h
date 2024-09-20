b21141e8409258d6395e29067002f97071dbbeb9
bearmade <110791884+bearmade@users.noreply.github.com>
2024-09-20 18:27:33 -0500

    update

---
+++ Source/ParticleLifeSim/ParticleLifeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ParticleLifeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARTICLELIFESIM_API AParticleLifeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
