d07a5933bd6fa908517fee3ad3a7ff00f1d37bd8
bearmade <110791884+bearmade@users.noreply.github.com>
2024-09-20 19:14:25 -0500

    update

---
+++ Source/ParticleLifeSim/ParticleLifeHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "ParticleLifeHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PARTICLELIFESIM_API AParticleLifeHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
