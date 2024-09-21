4666910bab5fb8079dde08bafe65ee18b8562275
bearmade <110791884+bearmade@users.noreply.github.com>
2024-09-20 19:02:35 -0500

    fix view

---
+++ Source/ParticleLifeSim/ParticleLifeCharacter.cpp
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "GameFramework/CharacterMovementComponent.h"
+#include "ParticleLifeCharacter.h"
+
+// Sets default values
+AParticleLifeCharacter::AParticleLifeCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	// Disable gravity for the character
+    GetCharacterMovement()->GravityScale = 0.0f;
+
+}
+
+// Called when the game starts or when spawned
+void AParticleLifeCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+// Called every frame
+void AParticleLifeCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AParticleLifeCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+
+
